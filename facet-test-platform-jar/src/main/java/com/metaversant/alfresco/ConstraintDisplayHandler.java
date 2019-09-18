32b7e0fda2a578377390c40500a12f6184a82485
Jeff Potts <jeffpotts01@gmail.com>
2019-09-18 17:33:38 -0500

    Update facet label display handler to leverage the dictionary service instead of hardcoding the mapping between constraint values and props. Move the props to the repo messages.

--- facet-test-platform-jar/src/main/java/com/metaversant/alfresco/ConstraintDisplayHandler.java
+++ facet-test-platform-jar/src/main/java/com/metaversant/alfresco/ConstraintDisplayHandler.java
@@ -1,38 +1,61 @@
 package com.metaversant.alfresco;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
+import org.alfresco.repo.dictionary.constraint.ListOfValuesConstraint;
 import org.alfresco.repo.search.impl.solr.facet.handler.AbstractFacetLabelDisplayHandler;
 import org.alfresco.repo.search.impl.solr.facet.handler.FacetLabel;
+import org.alfresco.service.cmr.dictionary.Constraint;
+import org.alfresco.service.cmr.dictionary.ConstraintDefinition;
+import org.alfresco.service.cmr.dictionary.DictionaryService;
+import org.alfresco.service.cmr.dictionary.PropertyDefinition;
+import org.alfresco.service.namespace.QName;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.extensions.surf.util.ParameterCheck;
 
 public class ConstraintDisplayHandler extends AbstractFacetLabelDisplayHandler {
-    private final Map<String, String> labels;
+    private static Log logger = LogFactory.getLog(ConstraintDisplayHandler.class);
+
+    // Dependencies
+    private DictionaryService dictionaryService;
 
     public ConstraintDisplayHandler(Set<String> supportedFieldFacets) {
-        this(supportedFieldFacets, Collections.<String, String> emptyMap());
-    }
-
-    public ConstraintDisplayHandler(Set<String> supportedFieldFacets, Map<String, String> labels) {
         ParameterCheck.mandatory("supportedFieldFacets", supportedFieldFacets);
-
         this.supportedFieldFacets = Collections.unmodifiableSet(new HashSet<>(supportedFieldFacets));
-        this.labels = labels == null ? Collections.<String, String> emptyMap() : labels;
     }
 
     @Override
     public FacetLabel getDisplayLabel(String value) {
         String title = null;
+        logger.debug("Inside getDisplayLabel for: " + value);
 
-        if (labels.containsKey(value)) {
-            title = labels.get(value);
-        } else {
+        // iterate over the fields this handler supports
+        // for each one, ask the dictionary for its constraints
+        // if there is a list of values constraint, see if the data dictionary knows what the label is
+        // for the value passed in to the method
+        for (String prop : supportedFieldFacets) {
+            PropertyDefinition propDef = dictionaryService.getProperty(QName.createQName(prop.substring(1)));
+            List<ConstraintDefinition> constraints = propDef.getConstraints();
+            for (ConstraintDefinition constraintDef : constraints) {
+                Constraint constraint = constraintDef.getConstraint();
+                if (constraint instanceof ListOfValuesConstraint) {
+                    ListOfValuesConstraint lovConstraint = (ListOfValuesConstraint) constraint;
+                    title = lovConstraint.getDisplayLabel(value, dictionaryService);
+                    logger.debug("Label: " + title);
+                }
+            }
+        }
+
+        // if title is still null at this point, just use the value instead
+        if (title == null) {
             title = value;
         }
 
         return new FacetLabel(value, title, -1);
+    }
+
+    public void setDictionaryService(DictionaryService dictionaryService) {
+        this.dictionaryService = dictionaryService;
     }
 }
